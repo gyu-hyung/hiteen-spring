b03d71447f3ee2b85aee3001310b61764cfa30c9
JGH <gud5603@gmail.com>
2025-08-19 13:23:47 +0900

    redis uninstall

--- k8s/hteen-delete.sh
+++ k8s/hteen-delete.sh
@@ -5,17 +5,22 @@
 #
 #
 
-
-
 #!/bin/bash
 set -e
 
+# PostgreSQL
 kubectl delete deployment hiteen-postgres -n hiteen || true
 kubectl delete svc hiteen-postgres -n hiteen || true
 kubectl delete pvc hiteen-postgres-pvc -n hiteen || true
 
+# MongoDB
 kubectl delete deployment hiteen-mongo -n hiteen || true
 kubectl delete svc hiteen-mongo -n hiteen || true
 kubectl delete pvc hiteen-mongo-pvc -n hiteen || true
 
-echo "HITEEN PostgreSQL & MongoDB 리소스 삭제 완료!"
+# Redis
+kubectl delete deployment hiteen-redis -n hiteen || true
+kubectl delete svc hiteen-redis -n hiteen || true
+kubectl delete pvc hiteen-redis-pvc -n hiteen || true
+
+echo "HITEEN PostgreSQL, MongoDB & Redis 리소스 삭제 완료!"
