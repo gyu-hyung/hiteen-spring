1ece122da49ec9ee2cd8a8b65799d2c8a898aca3
JGH <gud5603@gmail.com>
2025-08-19 11:52:00 +0900

    websocket realtime, redis broadcast, cache

--- src/main/kotlin/kr/jiasoft/hiteen/feature/ws/WebSocketConfig.kt
+++ src/main/kotlin/kr/jiasoft/hiteen/feature/ws/WebSocketConfig.kt
@@ -1,27 +1,19 @@
-//package kr.jiasoft.hiteen.feature.ws
-//
-//import org.springframework.context.annotation.Configuration
-//import org.springframework.messaging.simp.config.MessageBrokerRegistry
-//import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker
-//import org.springframework.web.socket.config.annotation.StompEndpointRegistry
-//import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer
-//
-//@Configuration
-//@EnableWebSocketMessageBroker
-//class WebSocketConfig : WebSocketMessageBrokerConfigurer {
-//
-//    // 클라이언트가 연결할 WebSocket 엔드포인트 등록
-//    override fun registerStompEndpoints(registry: StompEndpointRegistry) {
-//        registry.addEndpoint("/ws") // ws://localhost:8080/ws
-//            .setAllowedOriginPatterns("*") // 개발 중엔 전체 허용
-//            .withSockJS() // SockJS fallback 지원 (옵션)
-//    }
-//
-//    // 메시지 브로커 설정
-//    override fun configureMessageBroker(registry: MessageBrokerRegistry) {
-//        // 서버에서 클라이언트로 보낼 prefix
-//        registry.enableSimpleBroker("/topic")
-//        // 클라이언트 → 서버 메시지 prefix
-//        registry.setApplicationDestinationPrefixes("/app")
-//    }
-//}
+package kr.jiasoft.hiteen.feature.ws
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.reactive.socket.server.WebSocketService
+import org.springframework.web.reactive.socket.server.support.WebSocketHandlerAdapter
+
+import org.springframework.web.reactive.socket.server.upgrade.ReactorNettyRequestUpgradeStrategy
+import org.springframework.web.reactive.socket.server.support.HandshakeWebSocketService
+
+@Configuration
+class WebSocketConfig {
+    @Bean
+    fun handlerAdapter(): WebSocketHandlerAdapter = WebSocketHandlerAdapter(webSocketService())
+
+    @Bean
+    fun webSocketService(): WebSocketService =
+        HandshakeWebSocketService(ReactorNettyRequestUpgradeStrategy())
+}
