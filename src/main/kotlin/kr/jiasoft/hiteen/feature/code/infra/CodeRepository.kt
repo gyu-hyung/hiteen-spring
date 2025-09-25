14e4c6997a5533cdea75d466ba749a700b1ae704
jogyuhyeong <gud5603@gmail.com>
2025-09-25 14:32:29 +0900

    code

--- src/main/kotlin/kr/jiasoft/hiteen/feature/code/infra/CodeRepository.kt
+++ src/main/kotlin/kr/jiasoft/hiteen/feature/code/infra/CodeRepository.kt
@@ -2,13 +2,21 @@ package kr.jiasoft.hiteen.feature.code.infra
 
 import kotlinx.coroutines.flow.Flow
 import kr.jiasoft.hiteen.feature.code.domain.CodeEntity
+import kr.jiasoft.hiteen.feature.code.dto.CodeWithAssetResponse
 import org.springframework.data.r2dbc.repository.Query
 import org.springframework.data.repository.kotlin.CoroutineCrudRepository
 
 interface CodeRepository : CoroutineCrudRepository<CodeEntity, Long> {
 
-    @Query("SELECT * FROM codes WHERE code_group = :group AND deleted_at IS NULL")
-    fun findByGroup(group: String): Flow<CodeEntity>
+    @Query("""
+        SELECT c.*, ca.uid
+        FROM codes c
+        left join code_assets ca on c.id = ca.code_id 
+        WHERE deleted_at IS null
+        AND c.code_group = :group
+        ORDER BY code_group, code
+    """)
+    fun findByGroup(group: String): Flow<CodeWithAssetResponse>
 
     @Query("SELECT code FROM codes WHERE code_group = :group ORDER BY code DESC LIMIT 1")
     suspend fun findLastCodeByGroup(group: String): String?
