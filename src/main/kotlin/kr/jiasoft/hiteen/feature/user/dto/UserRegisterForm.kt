fcd4df343907a32d9f067fbd63f920fa88014f59
jogyuhyeong <gud5603@gmail.com>
2025-10-17 10:59:22 +0900

    school Api return type, user join valid

--- src/main/kotlin/kr/jiasoft/hiteen/feature/user/dto/UserRegisterForm.kt
+++ src/main/kotlin/kr/jiasoft/hiteen/feature/user/dto/UserRegisterForm.kt
@@ -18,7 +18,7 @@ data class UserRegisterForm(
     var username: String? = null,
 
 //    @field:NotBlank(message = "이메일은 필수입니다.")
-    @field:Email(message = "올바른 이메일 형식이어야 합니다.")
+//    @field:Email(message = "올바른 이메일 형식이어야 합니다.")
     @param:Schema(description = "이메일 주소", example = "user@example.com")
     val email: String? = null,
 
