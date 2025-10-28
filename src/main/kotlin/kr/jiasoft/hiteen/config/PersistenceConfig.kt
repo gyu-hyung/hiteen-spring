565b55c13a195ec93409fc1455e3ae4a51751683
jogyuhyeong <gud5603@gmail.com>
2025-10-28 10:08:01 +0900

    clean

---
+++ src/main/kotlin/kr/jiasoft/hiteen/config/PersistenceConfig.kt
@@ -0,0 +1,22 @@
+package kr.jiasoft.hiteen.config
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.data.r2dbc.repository.config.EnableR2dbcRepositories
+import org.springframework.data.mongodb.repository.config.EnableReactiveMongoRepositories
+import org.springframework.data.redis.repository.configuration.EnableRedisRepositories
+
+@Configuration
+@EnableR2dbcRepositories(
+    basePackages = [
+        "kr.jiasoft.hiteen.feature.*"
+    ]
+)
+@EnableReactiveMongoRepositories(
+    basePackages = [
+        "kr.jiasoft.hiteen.feature.location.infra.db"
+    ]
+)
+@EnableRedisRepositories(
+    basePackages = []
+)
+class PersistenceConfig
