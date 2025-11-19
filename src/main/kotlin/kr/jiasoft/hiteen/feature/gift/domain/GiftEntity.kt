aeb0f15371c4ea45a52877fb7fab28851b1bfc24
jogyuhyeong <gud5603@gmail.com>
2025-11-19 18:39:41 +0900

    feat: 선물 도메인

--- src/main/kotlin/kr/jiasoft/hiteen/feature/gift/domain/GiftEntity.kt
+++ src/main/kotlin/kr/jiasoft/hiteen/feature/gift/domain/GiftEntity.kt
@@ -3,43 +3,53 @@ package kr.jiasoft.hiteen.feature.gift.domain
 import io.r2dbc.postgresql.codec.Json
 import io.swagger.v3.oas.annotations.media.Schema
 import org.springframework.data.annotation.Id
+import org.springframework.data.relational.core.mapping.Column
 import org.springframework.data.relational.core.mapping.Table
-import org.threeten.bp.OffsetDateTime
+import java.time.OffsetDateTime
 import java.util.UUID
 
-//선물 내역
+
+
 @Table("gift")
+@Schema(description = "선물 내역 엔티티")
 data class GiftEntity (
+
     @Id
-    @field:Schema(description = "선물 ID")
+    @field:Schema(description = "선물 고유 ID")
     val id: Long = 0,
-    @field:Schema(description = "선물 UID")
+
+    @field:Schema(description = "선물 고유 UID")
     val uid: UUID = UUID.randomUUID(),
 
-    @field:Schema(description = "상품 구분 POINT, VOUCHER, DELIVERY, ETC")
-    val type: String,
+    @field:Schema(description = "선물 구분 (Point, Voucher, Delivery, Etc)")
+    val type: GiftType,
 
-    @field:Schema(description = "선물 분류 CHALLENGE, ADMIN")
-    val category: String,
+    @field:Schema(description = "카테고리(친구, 챌린지, 관리자 등)")
+    val category: GiftCategory,
 
-    @field:Schema(description = "회원번호")
+    @field:Schema(description = "회원 번호")
+    @Column("user_id")
     val userId: Long,
 
-    @field:Schema(description = "결제번호")
-    val payId: Long,
+    @field:Schema(description = "결제 번호")
+    @Column("pay_id")
+    val payId: Long? = null,
 
-    @field:Schema(description = "선물 메모")
-    val memo: String,
+    @field:Schema(description = "메모")
+    val memo: String? = null,
 
-    @field:Schema(description = "받은 회원목록")
-    val users: Json,
+    @field:Schema(description = "받은 사용자 목록 JSON")
+    val users: Json? = null,
 
     @field:Schema(description = "생성일시")
+    @Column("created_at")
     val createdAt: OffsetDateTime = OffsetDateTime.now(),
 
     @field:Schema(description = "수정일시")
-    val updatedAt: OffsetDateTime,
+    @Column("updated_at")
+    val updatedAt: OffsetDateTime? = null,
 
     @field:Schema(description = "삭제일시")
-    val deletedAt: OffsetDateTime?
+    @Column("deleted_at")
+    val deletedAt: OffsetDateTime? = null,
 )
