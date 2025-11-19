861382fff8a1db742aaf6858c5feba4181f468d9
jogyuhyeong <gud5603@gmail.com>
2025-11-19 09:32:51 +0900

    refactor: 회원정보 댓글 수, 투표 수++

--- src/main/kotlin/kr/jiasoft/hiteen/feature/relationship/dto/RelationshipCounts.kt
+++ src/main/kotlin/kr/jiasoft/hiteen/feature/relationship/dto/RelationshipCounts.kt
@@ -7,6 +7,10 @@ data class RelationshipCounts (
 
     @param:Schema(description = "게시글 수", example = "10")
     val postCount: Int = 0,
+    @param:Schema(description = "투표 수", example = "10")
+    val voteCount: Int = 0,
+    @param:Schema(description = "댓글 수", example = "10")
+    val commentCount: Int = 0,
     @param:Schema(description = "팔로워 수", example = "10")
     val followerCount: Int = 0,
     @param:Schema(description = "팔로잉 수", example = "10")
