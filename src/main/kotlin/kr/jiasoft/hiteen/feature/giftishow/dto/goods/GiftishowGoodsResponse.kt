f2086adb65db40cb1dc0f547c8b7fc8cde05146b
jogyuhyeong <gud5603@gmail.com>
2025-11-20 19:09:12 +0900

    feat: giftishow import++

--- src/main/kotlin/kr/jiasoft/hiteen/feature/giftishow/dto/goods/GiftishowGoodsResponse.kt
+++ src/main/kotlin/kr/jiasoft/hiteen/feature/giftishow/dto/goods/GiftishowGoodsResponse.kt
@@ -4,9 +4,4 @@ data class GiftishowGoodsResponse(
     val code: String,
     val message: String?,
     val result: GoodsResult?
-) {
-    data class Result(
-        val listNum: Int,
-        val goodsList: List<GoodsDto>
-    )
-}
+)
