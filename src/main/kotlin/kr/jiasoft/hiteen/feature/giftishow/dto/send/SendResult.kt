8211e4e31ec2b763286fca5c196379136b2321fc
jogyuhyeong <gud5603@gmail.com>
2025-11-21 16:56:38 +0900

    fix: message 종류 file 추가

---
+++ src/main/kotlin/kr/jiasoft/hiteen/feature/giftishow/dto/send/SendResult.kt
@@ -0,0 +1,8 @@
+package kr.jiasoft.hiteen.feature.giftishow.dto.send
+
+data class SendResult(
+    val pinNo: String?,
+    val couponImgUrl: String?,
+    val tr_id: String,
+    val goodsName: String?
+)
