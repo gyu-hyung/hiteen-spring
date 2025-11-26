2866f8a2b271db022c28702da37222082fc285d8
jogyuhyeong <gud5603@gmail.com>
2025-11-26 09:56:52 +0900

    fix: cache test

--- src/main/kotlin/kr/jiasoft/hiteen/config/RedisConfig.kt
+++ src/main/kotlin/kr/jiasoft/hiteen/config/RedisConfig.kt
@@ -1,8 +1,10 @@
 package kr.jiasoft.hiteen.config
 
+import org.springframework.boot.CommandLineRunner
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.data.redis.connection.ReactiveRedisConnectionFactory
+import org.springframework.data.redis.connection.RedisConnectionFactory
 import org.springframework.data.redis.core.ReactiveStringRedisTemplate
 import org.springframework.data.redis.listener.ReactiveRedisMessageListenerContainer
 
@@ -15,6 +17,12 @@ class RedisConfig {
     @Bean
     fun redisListenerContainer(cf: ReactiveRedisConnectionFactory) =
         ReactiveRedisMessageListenerContainer(cf)
+
+    @Bean
+    fun checkRedis(redisConnectionFactory: RedisConnectionFactory) = CommandLineRunner {
+        println("🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Redis Connection: " + redisConnectionFactory::class.simpleName)
+    }
+
 }
 
 
