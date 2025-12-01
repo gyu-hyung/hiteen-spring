500f04b28e000db15a541b7ae977225405696e11
jogyuhyeong <gud5603@gmail.com>
2025-12-01 17:45:36 +0900

    fix: batch manual

--- src/main/kotlin/kr/jiasoft/hiteen/feature/batch/BatchManualController.kt
+++ src/main/kotlin/kr/jiasoft/hiteen/feature/batch/BatchManualController.kt
@@ -1,5 +1,6 @@
 package kr.jiasoft.hiteen.feature.batch
 
+import org.springframework.security.access.prepost.PreAuthorize
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
@@ -11,16 +12,19 @@ class BatchManualController (
 ){
 
     @PostMapping("/schoolFoodImport")
+    @PreAuthorize("hasRole('ADMIN')")
     suspend fun schoolFoodImport(){
         batchService.schoolFoodImport()
     }
 
     @PostMapping("/schoolImport")
+    @PreAuthorize("hasRole('ADMIN')")
     suspend fun schoolImport(){
         batchService.schoolImport()
     }
 
     @PostMapping("/autoManageSeasons")
+    @PreAuthorize("hasRole('ADMIN')")
     suspend fun autoManageSeasons(){
         batchService.autoManageSeasons()
     }
