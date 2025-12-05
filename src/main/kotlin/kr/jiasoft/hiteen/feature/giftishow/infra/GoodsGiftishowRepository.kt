108485ba5815cbb7a0cfe8f830a3b3c4661a58ae
jogyuhyeong <gud5603@gmail.com>
2025-12-05 10:03:02 +0900

    refactor: 선물함 굿즈정보++

--- src/main/kotlin/kr/jiasoft/hiteen/feature/giftishow/infra/GoodsGiftishowRepository.kt
+++ src/main/kotlin/kr/jiasoft/hiteen/feature/giftishow/infra/GoodsGiftishowRepository.kt
@@ -1,5 +1,6 @@
 package kr.jiasoft.hiteen.feature.giftishow.infra
 
+import kotlinx.coroutines.flow.Flow
 import kr.jiasoft.hiteen.feature.giftishow.domain.GoodsGiftishowEntity
 import org.springframework.data.r2dbc.repository.Query
 import org.springframework.data.repository.kotlin.CoroutineCrudRepository
@@ -7,6 +8,8 @@ import org.springframework.data.repository.kotlin.CoroutineCrudRepository
 interface GiftishowGoodsRepository : CoroutineCrudRepository<GoodsGiftishowEntity, Long> {
 
     suspend fun findByGoodsCode(goodsCode: String): GoodsGiftishowEntity?
+
+    fun findAllByGoodsCodeIn(goodsCodes: List<String>): Flow<GoodsGiftishowEntity>
 
     @Query("UPDATE goods_giftishow SET del_yn = 1")
     suspend fun markAllDeleted()
