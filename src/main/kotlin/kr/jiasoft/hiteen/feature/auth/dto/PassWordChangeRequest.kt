5c3e4ef62a0d81cdc014caac919cbaa4ac152508
jogyuhyeong <gud5603@gmail.com>
2025-12-10 13:55:44 +0900

    refactor: 비밀번호 규칙 변경

--- src/main/kotlin/kr/jiasoft/hiteen/feature/auth/dto/PassWordChangeRequest.kt
+++ src/main/kotlin/kr/jiasoft/hiteen/feature/auth/dto/PassWordChangeRequest.kt
@@ -2,17 +2,26 @@ package kr.jiasoft.hiteen.feature.auth.dto
 
 import io.swagger.v3.oas.annotations.media.Schema
 import jakarta.validation.constraints.NotBlank
+import jakarta.validation.constraints.Pattern
 import kr.jiasoft.hiteen.validation.ValidPassword
 
 @Schema(description = "비밀번호 변경 요청 DTO")
 data class PassWordChangeRequest(
     @field:NotBlank(message = "비밀번호는 필수입니다.")
-    @field:ValidPassword
+//    @field:ValidPassword
     @Schema(description = "비밀번호", example = "P@ssw0rd!")
+    @field:Pattern(
+        regexp = "^(?=.*[A-Za-z])(?=.*\\d).{6,}$",
+        message = "비밀번호는 영문과 숫자를 포함한 6자 이상이어야 합니다."
+    )
     val oldPassword: String,
 
     @field:NotBlank(message = "비밀번호는 필수입니다.")
-    @field:ValidPassword
+//    @field:ValidPassword
     @Schema(description = "비밀번호", example = "P@ssw0rd!")
+    @field:Pattern(
+        regexp = "^(?=.*[A-Za-z])(?=.*\\d).{6,}$",
+        message = "비밀번호는 영문과 숫자를 포함한 6자 이상이어야 합니다."
+    )
     val newPassword: String,
 )
