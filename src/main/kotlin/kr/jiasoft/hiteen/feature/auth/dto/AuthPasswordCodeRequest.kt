dd30985cbdee253db78ee0f43ecac48564fe9dcb
jogyuhyeong <gud5603@gmail.com>
2025-12-30 10:52:10 +0900

    auth code request 객체

--- src/main/kotlin/kr/jiasoft/hiteen/feature/auth/dto/AuthPasswordCodeRequest.kt
+++ src/main/kotlin/kr/jiasoft/hiteen/feature/auth/dto/AuthPasswordCodeRequest.kt
@@ -4,12 +4,16 @@ import io.swagger.v3.oas.annotations.media.Schema
 import jakarta.validation.constraints.NotBlank
 import jakarta.validation.constraints.Pattern
 
-data class AuthCodeRequest(
+data class AuthPasswordCodeRequest(
 
     @param:Schema(description = "휴대폰번호", example = "01012345678")
     @field:NotBlank(message = "휴대폰번호를 입력해줘~")
     @field:Pattern(regexp = "^[0-9]{9,20}$", message = "올바른 휴대폰번호 형식이 아냐~")
     val phone: String,
+
+    @param:Schema(description = "닉네임", example = "홍길동")
+    @field:NotBlank(message = "휴대폰번호를 입력해줘~")
+    val nickName: String,
 
     @param:Schema(description = "인증번호 타입", example = "User")
     val type: String? = "User"
