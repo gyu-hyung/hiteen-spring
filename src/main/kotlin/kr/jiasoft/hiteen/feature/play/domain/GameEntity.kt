07c00fc38c1ed2cc8c07c83a0c1c048e4c6a3805
jogyuhyeong <gud5603@gmail.com>
2026-01-06 14:12:44 +0900

    feat: date format

--- src/main/kotlin/kr/jiasoft/hiteen/feature/play/domain/GameEntity.kt
+++ src/main/kotlin/kr/jiasoft/hiteen/feature/play/domain/GameEntity.kt
@@ -1,5 +1,6 @@
 package kr.jiasoft.hiteen.feature.play.domain
 
+import com.fasterxml.jackson.annotation.JsonFormat
 import org.springframework.data.annotation.Id
 import org.springframework.data.relational.core.mapping.Table
 import java.time.OffsetDateTime
@@ -12,7 +13,10 @@ data class GameEntity(
     val name: String,
     val description: String? = null,
     val status: String = "ACTIVE",
+    @field:JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy.MM.dd HH:mm:ss")
     val createdAt: OffsetDateTime = OffsetDateTime.now(),
+    @field:JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy.MM.dd HH:mm:ss")
     val updatedAt: OffsetDateTime? = null,
+    @field:JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy.MM.dd HH:mm:ss")
     val deletedAt: OffsetDateTime? = null,
 )
