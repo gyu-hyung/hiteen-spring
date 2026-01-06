8f0ce9539ed63ac96fa60637c5560df7dc0bc691
jogyuhyeong <gud5603@gmail.com>
2026-01-06 15:35:35 +0900

    clean

--- src/main/kotlin/kr/jiasoft/hiteen/admin/infra/AdminBoardRepository.kt
+++ src/main/kotlin/kr/jiasoft/hiteen/admin/infra/AdminBoardRepository.kt
@@ -2,13 +2,40 @@ package kr.jiasoft.hiteen.admin.infra
 
 import kr.jiasoft.hiteen.feature.board.domain.BoardEntity
 import kotlinx.coroutines.flow.Flow
+import org.springframework.data.r2dbc.repository.Query
+import org.springframework.data.repository.kotlin.CoroutineCrudRepository
 
-interface AdminBoardRepository {
+interface AdminBoardRepository : CoroutineCrudRepository<BoardEntity, Long> {
 
-    suspend fun save(entity: BoardEntity): BoardEntity
 
-    suspend fun findById(id: Long): BoardEntity?
 
+    @Query("""
+        SELECT COUNT(*)
+        FROM question_2 q
+        WHERE
+            (
+                :search IS NULL
+                OR (
+                    :searchType = 'ALL' AND (
+                        q.question ILIKE CONCAT('%', :search, '%')
+                        OR q.answer ILIKE CONCAT('%', :search, '%')
+                    )
+                )
+                OR (:searchType = 'question' AND q.question ILIKE CONCAT('%', :search, '%'))
+                OR (:searchType = 'answer' AND q.answer ILIKE CONCAT('%', :search, '%'))
+            )
+            AND (
+                :status IS NULL OR :status = 'ALL'
+                OR (:status = 'ACTIVE' AND q.status = 1)
+                OR (:status = 'INACTIVE' AND q.status = 0)
+            )
+            AND (
+                :type IS NULL OR :type = 'ALL'
+                OR (:type = '1' AND q.type = 1)
+                OR (:type = '2' AND q.type = 2)
+                OR (:type = '3' AND q.type = 3)
+            )
+    """)
     fun listByPage(
         page: Int,
         size: Int,
@@ -19,6 +46,40 @@ interface AdminBoardRepository {
         category: String?,
     ): Flow<BoardEntity>
 
+
+    
+    @Query("""
+        SELECT q.*
+        FROM question_2 q
+        WHERE
+            (
+                :search IS NULL
+                OR (
+                    :searchType = 'ALL' AND (
+                        q.question ILIKE CONCAT('%', :search, '%')
+                        OR q.answer ILIKE CONCAT('%', :search, '%')
+                    )
+                )
+                OR (:searchType = 'question' AND q.question ILIKE CONCAT('%', :search, '%'))
+                OR (:searchType = 'answer' AND q.answer ILIKE CONCAT('%', :search, '%'))
+            )
+            AND (
+                :status IS NULL OR :status = 'ALL'
+                OR (:status = 'ACTIVE' AND q.status = 1)
+                OR (:status = 'INACTIVE' AND q.status = 0)
+            )
+            AND (
+                :type IS NULL OR :type = 'ALL'
+                OR (:type = '1' AND q.type = 1)
+                OR (:type = '2' AND q.type = 2)
+                OR (:type = '3' AND q.type = 3)
+            )
+        ORDER BY
+            q.status DESC,
+            CASE WHEN :order = 'DESC' THEN q.created_at END DESC,
+            CASE WHEN :order = 'ASC' THEN q.created_at END ASC
+        LIMIT :size OFFSET (:page - 1) * :size
+    """)
     suspend fun totalCount(
         search: String?,
         searchType: String,
